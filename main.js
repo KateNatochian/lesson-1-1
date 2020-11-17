4bec88de66ee0ef1ceeb26d5b79dc28bf4d70eaa
Katerine Natochina <72230900+KateNatochian@users.noreply.github.com>
2020-11-17 22:59:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let a = prompt('a = ', 0);
+a1 = parseInt(a);
+console.log('a =', a1);
+let b = prompt('b = ', 0);
+b1 = parseInt(b);
+console.log('b =', b1);
+let c = prompt('c = ', 0);
+c1 = parseInt(c);
+console.log('c =', c1);
+
+sum = a1 + b1 + c1;
+console.log(sum);
+
+if (a1 % 2 == 0) {
+  console.log('переменная а - четное число');
+}
+else {
+  console.log('переменная а - нечетное число')
+}
+
+if (b1 % 2 == 0) {
+  console.log('переменная b - четное число');
+}
+else {
+  console.log('переменная b - нечетное число')
+}
+
+if (c1 % 2 == 0) {
+  console.log('переменная c - четное число');
+}
+else {
+  console.log('переменная c - нечетное число')
+}
+
+
+
+
